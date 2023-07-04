03fb0f2ebcb676ff1f62f391715eb3a759bc58a8
t-k- <clock126@126.com>
2023-07-04 05:28:46 +0000

    deepspeed test_llama.py!

--- src/test_llama.py
+++ src/test_llama.py
@@ -1,19 +1,38 @@
+# export CUDA_VISIBLE_DEVICES=4,5
+# run with either:
+# (1) python test_llama.py ~/llama-models/7B-hgf/ --direct_inference
+# or
+# (2) deepspeed  --num_gpus 2 test_llama.py ~/llama-models/7B-hgf/
+import os
 import fire
 import torch
 import gradio as gr
+import deepspeed
 from transformers import LlamaTokenizer, LlamaForCausalLM, GenerationConfig
 
-def main(model_path):
+def main(model_path, local_rank=0, world_size=1, direct_inference=False):
     tokenizer = LlamaTokenizer.from_pretrained(model_path)
     print('Loading weights')
     model = LlamaForCausalLM.from_pretrained(
         model_path,
-        torch_dtype=torch.float16,
-        #device_map="auto", # buggy!!!
+
+        torch_dtype=torch.half,
+        # RuntimeError: "addmm_impl_cpu_" not implemented for 'Half'
+
+        #device_map="auto", # buggy!!! And we should not use both
+        # device_map="auto" and deepspeed.init_inference() together.
+        # The former is supported by Accelerate:
+        # https://github.com/microsoft/DeepSpeed/issues/3028
     )
-    model.to('cuda')
+
+    print('RANK:', local_rank, '/', world_size)
+    model.to(f'cuda:{local_rank}')
+
     model.eval()
     device = model.device
+    print('DEV:', device)
+
+    model = deepspeed.init_inference(model, mp_size=world_size, dtype=torch.half)
 
     def inference(prompt='My name is Mariama, my favorite'):
         print('prompt:', prompt)
@@ -25,12 +44,15 @@ def main(model_path):
                 max_new_tokens=128
             )
         output = tokenizer.decode(generation_output[0])
-        print('output:', output)
+        print(f'rank#{local_rank} output:', output)
         return output
 
-    inference()
-    #iface = gr.Interface(fn=inference, inputs="text", outputs="text")
-    #iface.launch(share=True)
+    if local_rank == 0:
+        if direct_inference:
+            inference()
+        else:
+            iface = gr.Interface(fn=inference, inputs="text", outputs="text")
+            iface.launch(share=True)
 
 if __name__ == '__main__':
     fire.Fire(main)
