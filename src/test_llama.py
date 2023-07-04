5d2025f8e70fe7d01dce056c4d6b4288ddde13f6
t-k- <clock126@126.com>
2023-07-04 03:56:56 +0000

    update test_llama.py

--- src/test_llama.py
+++ src/test_llama.py
@@ -1,34 +1,37 @@
 import fire
+import torch
 import gradio as gr
 from transformers import LlamaTokenizer, LlamaForCausalLM, GenerationConfig
 
 def main(model_path):
     tokenizer = LlamaTokenizer.from_pretrained(model_path)
     print('Loading weights')
-    model = LlamaForCausalLM.from_pretrained(model_path)#, torch_dtype=torch.float16)
-    device = model.device
-    print(device)
-    #model.half()  # seems to fix bugs for some users.
+    model = LlamaForCausalLM.from_pretrained(
+        model_path,
+        torch_dtype=torch.float16,
+        device_map="auto",
+    )
+    model.eval()
+    device = 'cuda'
+    model.half()  # seems to fix bugs for some users.
 
-    def inference(prompt='hello!'):
+    def inference(prompt='hello! we are'):
         print('prompt:', prompt)
         inputs = tokenizer(prompt, return_tensors="pt")
         input_ids = inputs["input_ids"].to(device)
-        generation_config = GenerationConfig()
         with torch.no_grad():
             generation_output = model.generate(
                 input_ids=input_ids,
-                generation_config=generation_config,
                 return_dict_in_generate=True,
                 output_scores=True
             )
-        s = generation_output.sequences[0]
-        output = tokenizer.decode(s)
+        output = tokenizer.decode(generation_output.sequences[0])
         print('output:', output)
         return output
 
-    iface = gr.Interface(fn=inference, inputs="text", outputs="text")
-    iface.launch(share=True)
+    inference()
+    #iface = gr.Interface(fn=inference, inputs="text", outputs="text")
+    #iface.launch(share=True)
 
 if __name__ == '__main__':
     fire.Fire(main)
